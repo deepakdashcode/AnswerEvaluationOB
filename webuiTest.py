1f91032b8f1e48e45d45a7c149fef0cbf726d26a
deepakdashcode <dipudash.2003@gmail.com>
2024-01-22 23:46:55 +0530

    streamlit

--- webuiTest.py
+++ webuiTest.py
@@ -4,13 +4,14 @@ from use_gemini import getOutput
 
 def getResponse(ques: str, ans: str, mark: int):
     prompt = (f'I want you to help me evaluate the answers of my students and provide appropriate marks'
-              f'The Question is'
+              f'The Question is\n'
               f'{ques}'
               f'\n'
-              f'And the answer written is'
+              f'And the answer written is\n'
               f'{ans}'
               f'\n'
-              f'The total marks are {mark}'
+              f'The total marks are {mark}\n'
+              f'Be a little liberal with the markings, and also analyze it with respect to organizational behaviour if possible\n'
               f'How much should he get. Simply Give me a number. No explanations required')
     return getOutput(prompt)
 
